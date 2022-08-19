ee2b6a4c5f8d2dd9cd8878f20a98f31e91a7da35
Marwan Mohamed <marawan.mohamed.refaei@gmail.com>
2022-08-19 20:28:55 +0200

    commit message2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Marwan Mohamed</h1>
-        <h5 className='text-light'>FrontEnd Developer</h5>
+        <h5 className='text-light'>Full-stack Developer</h5>
         <CTA/>
         <HeaderSocials/>
         <div className='me'>
